1f858388585e3fdb029403848add6400920783fc
Mauro-Cassoni <mauro.cassoni.94@gmail.com>
2023-10-17 16:55:19 +0200

    result page

---
+++ Results Page/js/script.js
@@ -0,0 +1,31 @@
+let correct = 66.7; 
+
+let wrong = 100 - correct;
+
+let data = [wrong, correct];
+
+let labels = ['Wrong', 'Correct'];
+
+let colors = ['#D20094', '#00FFFF'];
+let borderColor = ['#D20094', '#00FFFF'];
+
+let ctx = document.getElementById('myDoughnutChart').getContext('2d');
+let myDoughnutChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        datasets: [{
+            data: data,
+            backgroundColor: colors,
+            borderColor: borderColor,
+        }]
+    },
+    options: {
+        responsive: false,
+        cutout: 120,
+    }
+});
+
+ctx.shadowColor = 'rgba(0, 0, 0, 0.5)';
+ctx.shadowBlur = 20;
+ctx.shadowOffsetX = 0;
+ctx.shadowOffsetY = 0;
