672ae7cfde585484d7596a3c9c8763b2f9742859
Your Name <you@example.com>
2023-10-18 10:29:00 +0200

    checkbox

---
+++ welcomePagejs/script.js
@@ -0,0 +1,6 @@
+let checkbox = document.querySelector(".check-welcome-page")
+
+checkbox.addEventListener('click',function(){
+let selezione = document.createElement('button')
+checkbox.append(selezione)
+});
