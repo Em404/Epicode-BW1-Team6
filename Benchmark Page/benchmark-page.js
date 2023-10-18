6d3c888479f2fc303fb62f18b1419bccfd9e8982
Em404 <emanuele.macchiarulo.1998@gmail.com>
2023-10-18 14:19:21 +0200

    added selectAnswer, checkAnswer and button next

--- Benchmark Page/benchmark-page.js
+++ Benchmark Page/benchmark-page.js
@@ -1,80 +1,107 @@
 let timeLeft = 60;
-let timer = document.getElementById('timeLeft');
+let timer = document.getElementById("timeLeft");
 
 function isTimeLeft() {
-	return timeLeft > -1;
+  return timeLeft > -1;
 }
 
 function runTimer(timerElement) {
-	const timerCircle = timerElement.querySelector('svg > circle + circle');
-	timerElement.classList.add('animatable');
-	timerCircle.style.strokeDashoffset = 1;
+  const timerCircle = timerElement.querySelector("svg > circle + circle");
+  timerElement.classList.add("animatable");
+  timerCircle.style.strokeDashoffset = 1;
 
-	let countdownTimer = setInterval(function () {
-		if (isTimeLeft()) {
-			const timeRemaining = timeLeft--;
-			const normalizedTime = (60 - timeRemaining) / 60;
-			// for clockwise animation
-			// const normalizedTime = (timeRemaining - 60) / 60;
-			timerCircle.style.strokeDashoffset = normalizedTime;
-			timer.innerHTML = timeRemaining;
-		} else {
-			clearInterval(countdownTimer);
-			timerElement.classList.remove('animatable');
-		}
-	}, 1000);
+  let countdownTimer = setInterval(function () {
+    if (isTimeLeft()) {
+      const timeRemaining = timeLeft--;
+      const normalizedTime = (60 - timeRemaining) / 60;
+      // for clockwise animation
+      // const normalizedTime = (timeRemaining - 60) / 60;
+      timerCircle.style.strokeDashoffset = normalizedTime;
+      timer.innerHTML = timeRemaining;
+    } else {
+      clearInterval(countdownTimer);
+      timerElement.classList.remove("animatable");
+    }
+  }, 1000);
 }
 
-runTimer(document.querySelector('.timer'));
+runTimer(document.querySelector(".timer"));
 
-fetch('https://opentdb.com/api.php?amount=10&category=18&difficulty=easy')
-	.then(res => res.json())
-	.then(el => {
-		const arrayQuestions = el.results;
+fetch("https://opentdb.com/api.php?amount=10&category=18&difficulty=easy")
+  .then((res) => res.json())
+  .then((el) => {
+    const arrayQuestions = el.results;
 
-		function getRandom(nQuestions) {
-			const numRandom = Math.floor(Math.random() * nQuestions);
-			return numRandom;
+    function getRandom(nQuestions) {
+      const numRandom = Math.floor(Math.random() * nQuestions);
+      return numRandom;
+    }
+
+    function printQuestion(nQuestions) {
+      const question = document.querySelector(".question > p");
+			const index = getRandom(arrayQuestions.length);
+      const myQuestion = arrayQuestions[index];
+      question.innerHTML = myQuestion.question;
+			arrayQuestions.splice(index, 1);
+      return myQuestion, printAnswers(myQuestion);
+    }
+
+    function printAnswers(objQuestion) {
+      const arrayAnswers = [];
+      const divAnswersConteiner = document.querySelector(".wrap-answers");
+
+      arrayAnswers.push(objQuestion.correct_answer);
+      objQuestion.incorrect_answers.forEach((el) => {
+        arrayAnswers.push(el);
+      });
+      arrayAnswers.sort();
+
+      for (const answr of arrayAnswers) {
+        const divAnswer = document.createElement("div");
+        divAnswer.classList.add("answer");
+        divAnswer.innerHTML = answr;
+        divAnswersConteiner.append(divAnswer);
+      }
+
+			selectAnswer(objQuestion);
+
+      // return console.log(objQuestion);
+    }
+
+		const selectAnswer = (obj) => {
+			const answers = document.querySelectorAll(".answer");
+      for (const answer of answers) {
+        answer.addEventListener("click", () => {
+          answers.forEach((a) => a.classList.remove("bg"));
+          answer.classList.add("bg");
+					console.log(answer.textContent);
+					const selectedAnswer = answer.textContent
+
+					checkAnswer(selectedAnswer, obj);
+
+					// return selectedAnswer
+        });
+      }
 		}
 
-		function printQuestion(nQuestions) {
-			const question = document.querySelector('.question > p');
-			const myQuestion = arrayQuestions[getRandom(arrayQuestions.length)];
-			question.innerHTML = myQuestion.question;
-			return myQuestion, printAnswers(myQuestion);
-		}
+		let countCorrect = 0;
+		let countIncorrect = 0;
 
-		function printAnswers(objQuestion) {
-			const arrayAnswers = [];
-			const divAnswersConteiner = document.querySelector('.wrap-answers');
-
-			arrayAnswers.push(objQuestion.correct_answer);
-			objQuestion.incorrect_answers.forEach(el => {
-				arrayAnswers.push(el);
-			});
-			arrayAnswers.sort();
-
-			for (const answr of arrayAnswers) {
-				const divAnswer = document.createElement('div');
-				divAnswer.classList.add('answer');
-				divAnswer.innerHTML = answr;
-				divAnswersConteiner.append(divAnswer);
+		const checkAnswer = (answerToCheck, obj) => {
+			const correctAnswer = obj.correct_answer
+			console.log(correctAnswer);
+			if(answerToCheck == correctAnswer) {
+				countCorrect +=1
+			} else {
+				countIncorrect += 1
 			}
-
-			const answers = document.querySelectorAll('.answer');
-
-			for (const answer of answers) {
-				answer.addEventListener('click', () => {
-					console.log('hello');
-					answer.classList.add('bg');
-				});
-				console.log(answer);
-			}
-
-			return console.log(arrayAnswers);
 		}
 
+		const next = document.querySelector('.btn-next')
+		next.addEventListener("click", () => {
+			checkAnswer();
+		})
 
+    printQuestion(arrayQuestions.length);
+  });
 
-		printQuestion(arrayQuestions.length);
-	});
