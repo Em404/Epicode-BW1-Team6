41c902a304f0dc2a9433a1a9a3e327853c98a6b5
giorgiotopa <145937688+giorgiotopa@users.noreply.github.com>
2023-10-18 11:33:12 +0200

    casella checkbox

--- welcomePagejs/script.js
+++
@@ -1,6 +0,0 @@
-let checkbox = document.querySelector(".check-welcome-page")
-
-checkbox.addEventListener('click',function(){
-let selezione = document.createElement('button')
-checkbox.append(selezione)
-});
