565c6a6e0106e5104a138d22e9cd8a9899f3b186
giorgiotopa <145937688+giorgiotopa@users.noreply.github.com>
2023-10-20 10:18:44 +0200

    clone Welcome page

--- welcomePagejs/script.js
+++ welcomePagejs/script.js
@@ -1,10 +1,34 @@
-let checkbox = document.getElementById('check-welcome-page');
-let button = document.getElementById('btnWelcomePage')
 
-checkbox.addEventListener('click', function () {
-    if (checkbox.checked) {
-        button.removeAttribute('disabled')
-    }else{
-        button.disabled = 'true'
+
+function cloneWelcomePage(){
+
+    // Seleziona il modello
+    const template = document.getElementsByTagName("template")[0].content;
+
+    // Clona il contenuto del modello
+    const clone = template.cloneNode(true);
+
+    // Aggiungi il clone al contenitore desiderato
+    const cloneContainer = document.getElementById("clone-Welcome-Page");
+    cloneContainer.appendChild(clone);
+
+    initCheckbox()
+  }
+
+
+  cloneWelcomePage();
+
+  function initCheckbox() {   
+      
+      let checkbox = document.getElementById('check-welcome-page');
+      let button = document.getElementById('btnWelcomePage')
+      
+      checkbox.addEventListener('click', function () {
+          if (checkbox.checked) {
+              button.removeAttribute('disabled')
+            }else{
+                button.disabled = 'true'
+            }
+        })
     }
-})
+        
